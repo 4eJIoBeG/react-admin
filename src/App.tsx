6897949b874211b8db0bedfb06488de119d94522
4eJIoBeG <pavelshashenko@gmail.com>
2024-02-03 22:41:37 +0300

    add pages examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-function App() {
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
 
+function App() {
   return (
-    <div>Hello World</div>
-  )
+    <div>
+      <Home />
+      <Products />
+      <Users />
+    </div>
+  );
 }
 
-export default App
+export default App;
