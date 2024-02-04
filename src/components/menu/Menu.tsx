d10d741984188fcb89a05c30f188b7a22f242f6a
4eJIoBeG <pavelshashenko@gmail.com>
2024-02-05 00:05:35 +0300

    did once cell in home content

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -7,7 +7,7 @@ const Menu = () => {
     <div className="menu">
       {menu.map((item) => (
         <div className="item" key={item.id}>
-          <span className="title">{item.title.toUpperCase()}</span>
+          <span className="title">{item.title}</span>
           {item.listItems.map((listItem) => (
             <Link to="/" className="listItem" key={listItem.id}>
               <img src={listItem.icon} alt={listItem.title} />
